95ff43aed7d5862bbbe3dccc0ee5d4ac4928499f
atj3097 <adamthomasjackson3@gmail.com>
2021-10-17 02:18:00 -0400

    Created admin page to mint nft

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button'; 
 import SignIn from './components/SignInPage';  
-import Dashboard from './pages/Dashboard/Dashboard';
+import Dashboard from './pages/Dashboard/Dashboard'; 
+import WalletForm from './components/AdminPageF/WalletForm';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+import AdminPage from './components/AdminPageF/AdminPage';
 
 function App() {
   return ( 
     <Router>   
       
       <div className = "App">  
-      
+
       <Route path="/signin" component={SignIn}/>  
-      <Route path="/dashboard" component={Dashboard}/>
+      <Route path="/dashboard" component={Dashboard}/> 
+      <Route path="/admin" component={AdminPage}/>
       
       </div> 
 
