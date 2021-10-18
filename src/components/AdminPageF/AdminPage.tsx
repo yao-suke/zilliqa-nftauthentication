afc76ddc1043accb97e53b51d8acd6452bd7eecf
atj3097 <adamthomasjackson3@gmail.com>
2021-10-17 21:34:59 -0400

    Able to connect the wallet to the webpage nowgit add .git add .

--- src/components/AdminPageF/AdminPage.tsx
+++ src/components/AdminPageF/AdminPage.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -14,7 +14,8 @@ import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddressForm from './AddressForm';
 import WalletForm from './WalletForm';
-import Review from './Review';
+import Review from './Review'; 
+import ContextContainer from "../../functions/contextContainer"; 
 
 function Copyright() {
   return (
@@ -47,7 +48,70 @@ function getStepContent(step: number) {
 const theme = createTheme();
 
 export default function AdminPage() {
-  const [activeStep, setActiveStep] = React.useState(0);
+  const [activeStep, setActiveStep] = React.useState(0); 
+  const [zilPayCheck, setZilPayCheck] = useState<number>(0);
+    const {
+        zilPay,
+        setZilPay,
+        error,
+        setError,
+        setContract,
+        setContractState,
+        setCurrentBlockNumber,
+    } = ContextContainer.useContainer();
+
+    const getContractState = async () => {
+        const contract = await zilPay.contracts.at(
+            process.env.REACT_APP_SMART_CONTRACT_ADDRESS
+        );
+        setContract(contract);
+        const contractState = await contract.getState();
+        setContractState(contractState);
+    };
+
+    useEffect(() => {
+        if (error === false) return;
+        // @ts-ignore
+        const zilPay = window.zilPay;
+        if (zilPay === undefined) {
+            setError(true);
+            return;
+        }
+        setZilPay(zilPay);
+        setError(false);
+    }, [error]);
+
+    useEffect(() => {
+        if (error && zilPayCheck < 100) {
+            setZilPayCheck(zilPayCheck + 1);
+            setError(undefined);
+            return;
+        }
+        if (error !== false) return;
+
+        let block: any = undefined;
+        let account: any = undefined;
+
+        if (!zilPay.wallet.isConnect) return;
+        try {
+            block = zilPay.wallet.observableBlock().subscribe((block: any) => {
+                const blockNumber = parseInt(block.TxBlock.header.BlockNum);
+                setCurrentBlockNumber(blockNumber);
+                getContractState();
+            });
+            account = zilPay.wallet
+                .observableAccount()
+                .subscribe(() => getContractState());
+
+            getContractState();
+        } catch (e) {
+            console.log(e);
+        }
+        return function cleanup() {
+            block?.unsubscribe?.();
+            account?.unsubscribe?.();
+        };
+    }, [error]);
 
   const handleNext = () => {
     setActiveStep(activeStep + 1);
@@ -55,10 +119,27 @@ export default function AdminPage() {
 
   const handleBack = () => {
     setActiveStep(activeStep - 1);
-  };
+  }; 
 
-  return (
-    <ThemeProvider theme={theme}>
+  const connectZilPay = async () => {
+    await zilPay.wallet.connect();
+    window.location.reload();
+};
+
+  return zilPay.wallet.isConnect ? (  
+    
+    <ThemeProvider theme={theme}> 
+    {/* {!zilPay.wallet.isConnect && (
+                    <>
+                        <h4 className="text-xs font-semibold text-gray-500 tracking-wide uppercase py-4">
+                            ZilPay
+                        </h4>
+                        <Button variant = "contained" onClick={connectZilPay}> 
+                        Connect ZilPay
+                        </Button>
+                    </>
+                )}  */}
+
       <CssBaseline />
       <AppBar
         position="absolute"
@@ -71,14 +152,14 @@ export default function AdminPage() {
       >
         <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
-            Company name
+            NakomotoLabs
           </Typography>
         </Toolbar>
       </AppBar>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
           <Typography component="h1" variant="h4" align="center">
-            Checkout
+            New Employee Creation
           </Typography>
           <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
             {steps.map((label) => (
@@ -123,5 +204,24 @@ export default function AdminPage() {
         <Copyright />
       </Container>
     </ThemeProvider>
-  );
+  ) : ( 
+    <AppBar
+    position="absolute"
+    color="default"
+    elevation={0}
+    sx={{
+      position: 'relative',
+      borderBottom: (t) => `1px solid ${t.palette.divider}`,
+    }}
+  >
+    <Toolbar>
+      <Typography variant="h6" color="inherit" noWrap>
+        Lets Connect ZilPay! 
+      </Typography>
+    </Toolbar> 
+    <Button variant="contained" onClick={connectZilPay}> ZilPay Connection </Button>
+  </AppBar> 
+  
+    
+);
 }
